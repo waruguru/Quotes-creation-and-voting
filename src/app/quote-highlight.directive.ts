66a86ddb925e6f334e0ef475158c39a9d4cb1930
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-17 19:20:40 +0300

    Add HostListener in Quote directive

--- src/app/quote-highlight.directive.ts
+++ src/app/quote-highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appQuoteHighlight]'
 })
 export class QuoteHighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
 
 }

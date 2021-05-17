db634db504d64ee5ab5bf6cf92e10d9dc314149c
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-05-17 18:41:51 +0300

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { AddQuoteComponent } from './add-quote/add-quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    AddQuoteComponent,
+    QuoteDetailsComponent
   ],
   imports: [
     BrowserModule,
